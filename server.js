53c3ab6db27166c5b77494ddfd99f81716c75ab6
ryan-rutishauser <98336166+ryan-rutishauser@users.noreply.github.com>
2022-10-30 20:04:05 -0400

    13, 14 ,15
    Basic Web Server Redirects 26:16
    Node Package Manager (NPM) 27:28
    nodemon 30:58
    Since there weren't breaks in the video in this part I missed.

--- server.js
+++ server.js
@@ -3,37 +3,34 @@ const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
-const server = http
-  .createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
 
-    let path = './';
-    switch (req.url) {
-      case '/':
-        path += 'index.html';
-        res.statusCode = 200;
-        break;
-      case '/about':
-        path += 'about.html';
-        res.statusCode = 200;
-        break;
-      default:
-        // eslint-disable-next-line no-unused-vars
-        path += '404.html';
-        res.statusCode = 404;
-        break;
+  let path = './';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 301;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    } else {
+      res.end(data);
     }
-
-    fs.readFile(path, (err, data) => {
-      if (err) {
-        console.error(err);
-        res.end();
-      } else {
-        res.end(data);
-      }
-    });
   });
+});
 
 server.listen(PORT, () => console.log(`Server listening 
 on port ${PORT}`));
